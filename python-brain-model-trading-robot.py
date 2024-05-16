b575c4f484f9278512899a70ec077d31f5fefd30
CodeProSpecialist <droark28@hotmail.com>
2024-05-16 10:12:52 -0500

    updates

--- python-brain-model-trading-robot.py
+++ python-brain-model-trading-robot.py
@@ -260,8 +260,9 @@ def submit_sell_order(symbol, quantity):
 
     if position.qty != '0':
         bought_price = float(position.avg_entry_price)
-        # sell quickly when the market quickly changes for at least 1 penny more than the purchased price. 
-        if day_trade_count < 3 and current_price >= bought_price + 0.01:
+        # sell when the market quickly changes for +1% more than the purchased price.
+        #  current_price >= bought_price * 1.01:
+        if day_trade_count < 3 and current_price >= bought_price * 1.01:
             api2.submit_order(
                 symbol=symbol,
                 qty=quantity,
